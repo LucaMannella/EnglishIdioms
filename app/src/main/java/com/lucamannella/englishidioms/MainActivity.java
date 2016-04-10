86a24d86cb2bde60c822145881582d5d98eeada9
Luke <luca.mannella@outlook.it>
2016-04-10 23:20:48 +0200

    commit di prova file .xml e file .java

--- app/src/main/java/com/lucamannella/englishidioms/MainActivity.java
+++ app/src/main/java/com/lucamannella/englishidioms/MainActivity.java
@@ -1,7 +1,9 @@
 package com.lucamannella.englishidioms;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void tostamelo(View view) {
+        Toast.makeText(this, "Ciao sono un toast!", Toast.LENGTH_SHORT).show();
     }
 }
