9d535b12c0f0429b14ab1ae5866d8d89d4221ede
Luke <luca.mannella@outlook.it>
2016-04-11 17:09:54 +0200

    Basic implementation of GameActivity completed.

--- app/src/main/java/com/lucamannella/englishidioms/GameActivity.java
+++ app/src/main/java/com/lucamannella/englishidioms/GameActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 /**
@@ -11,10 +12,14 @@ import android.widget.Toast;
  */
 public class GameActivity extends AppCompatActivity {
 
+    int vet[] = {R.drawable.example, R.drawable.france_flag, R.drawable.italy_flag, R.drawable.germany_flag, R.drawable.spain_flag};
+    int i = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        ((ImageView)findViewById(R.id.rebus_picture)).setImageResource(vet[i++]);
     }
 
 
@@ -32,6 +37,10 @@ public class GameActivity extends AppCompatActivity {
 
     public void nextActivity(View view) {
         Toast.makeText(this, "Please, implement me! :(", Toast.LENGTH_SHORT).show();
+        findViewById(R.id.explanation_text_view).setVisibility(View.GONE);
+        findViewById(R.id.next_button).setVisibility(View.GONE);
+        ((ImageView)findViewById(R.id.rebus_picture)).setImageResource(vet[i]);
+        i = (i+1)%5;
     }
 
 }
