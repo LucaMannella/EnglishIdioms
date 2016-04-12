02230400163253b52b36d99b862106ef6e611447
Luke <luca.mannella@outlook.it>
2016-04-13 00:08:05 +0200

    JavaDoc was added on MainActivity.

--- app/src/main/java/com/lucamannella/englishidioms/MainActivity.java
+++ app/src/main/java/com/lucamannella/englishidioms/MainActivity.java
@@ -6,6 +6,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
+/**
+ * This activity is the main class of the application.
+ * Created by Luca on 09/04/2016.
+ * @author Luca
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -14,16 +19,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    /**
+     * This method will start the activity in which the user will play.
+     * @param view - The button that was pressed to start this method.
+     */
     public void startGameActivity(View view) {
         Intent intent = new Intent(this, GameActivity.class);
         startActivity(intent);
     }
 
+    /**
+     * This method will open the activity related to the language selection.
+     * @param view - The button that was pressed to start this method.
+     */
     public void startLanguageActivity (View view) {
         Intent intent = new Intent(this, LanguageActivity.class);
         startActivity(intent);
     }
 
+    /**
+     * This method will manage the google app invite.
+     * @param view - The button that was pressed to start this method.
+     */
     public void startAppInviteActivity (View view) {
         Toast.makeText(this, "Implement me! :(", Toast.LENGTH_SHORT).show();
     }
