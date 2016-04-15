d2c602fff0f140976fa874eeb34942bb5c268cc1
Luke <luca.mannella@outlook.it>
2016-04-15 21:05:50 +0200

    Documentation and comments were added.

--- app/src/main/java/com/lucamannella/englishidioms/GameOverActivity.java
+++ app/src/main/java/com/lucamannella/englishidioms/GameOverActivity.java
@@ -4,6 +4,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+/**
+ * This is the last activity of the game.
+ * Created by Luca on 11/04/2016.
+ * @author Luca
+ */
 public class GameOverActivity extends AppCompatActivity {
 
     private final static String eMail = "luca.mannella@studenti.polito.it";
@@ -14,11 +19,19 @@ public class GameOverActivity extends AppCompatActivity {
         setContentView(R.layout.activity_game_over);
     }
 
+    /**
+     * This method send an e-mail to the creator of the application (me :D)!
+     * @param view - The button that was pressed to start this method (not used).
+     */
     public void mailMe (View view) {
         String subject = getResources().getString(R.string.email_subject_feedback);
         Utility.sendEmail(eMail, subject, getResources().getString(R.string.email_feedback_text), this);
     }
 
+    /**
+     * This method terminates the activity.
+     * @param view - The button that was pressed to start this method (not used).
+     */
     public void goBackToMenu (View view) {
         finish();
     }
